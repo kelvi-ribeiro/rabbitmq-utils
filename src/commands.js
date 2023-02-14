4d30b7e98f185d1707400bfd7205083921e3a410
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2023-02-13 21:55:32 -0300

    Implementing a parameter to decide whether to save moved messages on disk or not

--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,11 @@
 const rabbitmqApi = require('./rabbitmq-api');
+const fm = require('./file-manipulation');
+const path = require('path');
+const os = require("os");
 require("dotenv").config();
 require("./string-extensions");
+const currentDate = new Date();
+const rabbitMqQueue = process.env.RABBITMQ_QUEUE;
 
 const moveMessages = async () => {
     const { data: { backing_queue_status: { len: queueLength } } } = await rabbitmqApi.getQueueMessagesCount();
@@ -14,9 +19,24 @@ const moveMessages = async () => {
     for (let index = 0; index < loopTimes; index++) {
         let { data: messages } = await rabbitmqApi.getQueueMessages(process.env.RABBITMQ_MAX_MESSAGES_FETCH);
         messages = messages.map(d => d.payload);
+        if (process.env.SAVE_MOVED_MESSAGES_ON_DISK) {
+            await saveMessagesOnDisk(messages)
+        }
         await rabbitmqApi.publishMessages(messages);
         await rabbitmqApi.clearQueueMessages(process.env.RABBITMQ_MAX_MESSAGES_FETCH);
     }
+}
+
+const saveMessagesOnDisk = async (messages) => {
+    const workplacePath = os.homedir() + path.sep + "rabbit-utils" + path.sep + "saved-messages";
+    fm.createDir(workplacePath)
+    const responseContent = messages.map(d => {
+        if (d.isJson()) {
+            return JSON.stringify(JSON.parse(d));
+        }
+        return d;
+    }).join(os.EOL);
+    fm.appendFile(`${workplacePath}${path.sep}${rabbitMqQueue}-${currentDate.getTime()}`, responseContent);
 }
 
 module.exports = {
--- src/file-manipulation.js
+++ src/file-manipulation.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
-const path = require('path');
 require("dotenv").config();
-const rabbitMqQueue = process.env.RABBITMQ_QUEUE;
 
-const createTempDir = () =>
-    fs.mkdir(__dirname + path.sep + ".temp", { recursive: true }, () => { });
+const createDir = (path) =>
+    fs.mkdirSync(path, { recursive: true }, () => { });
 
-const removeTempDir = () =>
-    fs.rm(__dirname + path.sep + ".temp", { recursive: true, force: true }, () => { });
+const removeDir = (path) =>
+    fs.rm(path, { recursive: true, force: true }, () => { });
 
-const appendFile = (responseContent) =>
-    fs.appendFileSync(`${__dirname}${path.sep}.temp${path.sep}${rabbitMqQueue}-messages.txt`, responseContent, { flags: 'a' });
+const appendFile = (path, responseContent) =>
+    fs.appendFileSync(path, responseContent, { flags: 'a' });
 
 module.exports = { 
-    createTempDir,
-    removeTempDir,
+    createDir,
+    removeDir,
     appendFile
 }
